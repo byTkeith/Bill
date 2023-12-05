b376bc110bc416790ccf5f0e4c8cfa12f81a59d3
Your Name <you@example.com>
2023-12-05 17:30:38 +0200

    used arrayList for items

--- Bill.java
+++ Bill.java
@@ -2,10 +2,11 @@
  * a program that checks if the shop has the products and prints out the total of all present products in the shop
  */
 import java.io.*;
-import java.itil.*;
+import java.util.*;
 import java.util.Scanner;
 public class Bill{
     public static void main(String[] args) {
+        ArrayList<String> storeGroceryList= new ArrayList<>();//arrayList to add the list of items in the store
         Scanner scanner =new Scanner(System.in);
         System.out.println("Enter the name of the products file:");
         String filename=scanner.nextLine();//to store the file name
@@ -14,6 +15,10 @@ public class Bill{
         try{
             File file=new File(filename);
             scanner=new Scanner(file);
+            while(scanner.hasNextLine()){//so long as there is still an occupied line add the values to storeGroceryList
+                storeGroceryList.add(scanner.nextLine());
+
+            }
         }catch(FileNotFoundException e){//to catch the error if the file is not found
             System.out.println("File not found askies");
         }finally{
