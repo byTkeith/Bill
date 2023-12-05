8bb912657fd61245c081c15b3254a6e64ffbafba
Your Name <you@example.com>
2023-12-05 17:10:41 +0200

    used scanner to read the file

--- Bill.java
+++ Bill.java
@@ -1,8 +1,20 @@
 /*author:Tendai K Nyevedzanai
  * a program that checks if the shop has the products and prints out the total of all present products in the shop
  */
+import java.io.*;
+import java.itil.*;
+import java.util.Scanner;
 public class Bill{
     public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Enter the name of the products file:");
+        String filename=scanner.nextLine();
+        System.out.println("Enter the shopping list:");
+        String clientList=scanner.nextLine();
+        try{
+            File file=new File(filename);
+            scanner=new Scanner(file);
+        }
         
     }
 }
