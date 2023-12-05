5e7abe2148216c955c224704fd4207954f953367
Your Name <you@example.com>
2023-12-05 16:53:29 +0200

    initial commit

---
+++ Bill.java
@@ -0,0 +1,8 @@
+/*author:Tendai K Nyevedzanai
+ * a program that checks if the shop has the products and prints out the total of all present products in the shop
+ */
+public class Bill{
+    public static void main(String[] args) {
+        
+    }
+}
